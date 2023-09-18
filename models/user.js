0495608df7448ca65979ce4a691795960cd0b9cf
Joshua <jhellst@gmail.com>
2023-09-18 12:03:57 -0700

    Started work on User model, including registration

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const { NotFoundError } = require("../expressError");
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
 /** User of the site. */
 
 class User {
@@ -9,11 +14,26 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (username,
+                             password,
+                             first_name,
+                             last_name,
+                             phone,
+                             join_at)
+         VALUES
+           ($1, $2, $3, $4, $5, current_timestamp)
+         RETURNING username, password, first_name, last_name, phone`,
+    [username, hashedPassword, first_name, last_name, phone]);
+
+return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+
   }
 
   /** Update last_login_at for user */
